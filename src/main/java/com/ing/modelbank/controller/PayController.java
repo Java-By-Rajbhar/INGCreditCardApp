489c77c9645ca2b9d8b6d46d49bd57ba656f73f3
sushil <sushilrajbhar2019@gmail.com>
2019-08-20 17:24:03 +0530

    added otp

--- src/main/java/com/ing/modelbank/controller/PayController.java
+++ src/main/java/com/ing/modelbank/controller/PayController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +21,11 @@ public class PayController {
 	PayServiceImpl payServiceImpl;
 	private static final Logger logger = LoggerFactory.getLogger(PayController.class);
 
-	@PostMapping("/pay/{customerId}")
-	public ResponseEntity<PayResponseDto> payNow(@PathVariable long customerId,CreditCardRequestDto creditCardRequestDto)
+	@PostMapping("/pay")
+	public ResponseEntity<PayResponseDto> payNow(CreditCardRequestDto creditCardRequestDto)
 	{
 		logger.info("inside payNow method of PayController class");
-		PayResponseDto response = payServiceImpl.pay(customerId, creditCardRequestDto);
+		PayResponseDto response = payServiceImpl.pay (creditCardRequestDto);
 		return new ResponseEntity<PayResponseDto>(response, HttpStatus.OK);
 
 	}
--- src/main/java/com/ing/modelbank/service/PayServiceImpl.java
+++ src/main/java/com/ing/modelbank/service/PayServiceImpl.java
@@ -1,53 +1,79 @@
 package com.ing.modelbank.service;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 import com.ing.modelbank.dto.CreditCardRequestDto;
 import com.ing.modelbank.dto.PayResponseDto;
 import com.ing.modelbank.entity.CreditCard;
+import com.ing.modelbank.entity.Otp;
 import com.ing.modelbank.exception.CreditCardDoesNotExistsException;
 import com.ing.modelbank.exception.WrongCreditCardDetailException;
 import com.ing.modelbank.repository.CreditCardRepository;
+import com.ing.modelbank.repository.OtpRepository;
 
 /**
  * 
  * @author Sushil
  *
  */
-public class PayServiceImpl implements PayService{
-	
+@Service
+public class PayServiceImpl implements PayService {
+
 	@Autowired
 	CreditCardRepository creditCardRepository;
-	private static final Logger logger  = LoggerFactory.getLogger(PayServiceImpl.class);
+
+	@Autowired
+	MailService mailService;
+
+	@Autowired
+	OtpRepository otpRepository;
+
+	private static final Logger logger = LoggerFactory.getLogger(PayServiceImpl.class);
 
 	@Override
-	public PayResponseDto pay(long customerId, CreditCardRequestDto creditCardRequestDto) {
+	public PayResponseDto pay(CreditCardRequestDto creditCardRequestDto) {
 		logger.info("inside pay method of PayServiceImpl class");
 		/* get credit card object customer id */
-		
-		CreditCard creditCardDetail  = creditCardRepository.findByCustomerCustomerId(customerId);
-		
-		if(creditCardDetail!=null)
-		{
-			if(creditCardDetail.getCardCardNumber().equals(creditCardRequestDto.getCardCardNumber()) && creditCardDetail.getCvv()==creditCardRequestDto.getCvv()&& creditCardDetail.getExpiryDate().equals(creditCardRequestDto.getExpiryDate())&&creditCardDetail.getCreditCardHolderName().equals(creditCardRequestDto.getCreditCardHolderName()))
-			{
-				/* call otp generate service to generate otp*/
-				return null;
-			}
-			else
-			{
-				throw new WrongCreditCardDetailException("Please pass right credit card detials !!!! ");
-			}
+
+		CreditCard creditCardDetail = creditCardRepository.findByCustomerCustomerId(creditCardRequestDto.getCustomerId());
+		logger.info("email  data={}", creditCardDetail.getCustomer().getEmail());
+
+		if (creditCardDetail != null) {
+			if (creditCardDetail.getCardCardNumber().equals(creditCardRequestDto.getCardCardNumber())
+					&& creditCardDetail.getCvv() == creditCardRequestDto.getCvv()
+					&& creditCardDetail.getExpiryDate().equals(creditCardRequestDto.getExpiryDate()) && creditCardDetail
+							.getCreditCardHolderName().equals(creditCardRequestDto.getCreditCardHolderName())) {
+				/* call otp generate service to generate otp */
+				Random rand = new Random();
+				long otp = 1000 + rand.nextInt(9999);
+
+				mailService.sendEmails(creditCardDetail.getCustomer().getEmail(), creditCardDetail.getCreditCardId(),
+						otp);
+				//create otp object
+				Otp otp1 =  new Otp();
+				otp1.setOtp(otp);
+				otp1.setReferenceId(creditCardDetail.getCreditCardId());
+				//save otp detail in table
+				otpRepository.save(otp1);
+				//generate response
+				PayResponseDto payResponseDto = new PayResponseDto();
+				payResponseDto.setStatusCode(HttpStatus.OK.value());
+				payResponseDto.setMessage("Please verify your otp");
 				
-		}
-		else
-		{
-			throw new CreditCardDoesNotExistsException("Credit card does not exist with customer id "+customerId);
+				return payResponseDto;
+			} else {
+				throw new WrongCreditCardDetailException("Please provide correct credit card detials !!!! ");
+			}
+
+		} else {
+			throw new CreditCardDoesNotExistsException("Customer ID " + creditCardRequestDto.getCustomerId() + " does not exist");
 		}
 	}
-	
-	
 
 }
