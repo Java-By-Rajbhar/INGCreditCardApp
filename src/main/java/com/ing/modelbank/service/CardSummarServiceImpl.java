a1833941c35d732bed5529c93d79ae594445f3c9
user1 <anuradha.krishnanov7@gmail.com>
2019-08-20 17:48:04 +0530

    payee controller.

--- src/main/java/com/ing/modelbank/service/CardSummarServiceImpl.java
+++ src/main/java/com/ing/modelbank/service/CardSummarServiceImpl.java
@@ -2,9 +2,12 @@ package com.ing.modelbank.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ing.modelbank.controller.PayController;
 import com.ing.modelbank.dto.CardDetailDto;
 import com.ing.modelbank.dto.CardSummryDto;
 import com.ing.modelbank.entity.CardSummary;
@@ -22,9 +25,12 @@ public class CardSummarServiceImpl implements CardSummaryService {
 	@Autowired
 	private CreditCardRepository creditCardRepository;
 	
+	private static final Logger logger = LoggerFactory.getLogger(CardSummarServiceImpl.class);
+	
 	@Override
 	public CardSummryDto cardSummary(Long customerId) {
 
+		logger.info("in card summary");
 		
 		CardSummryDto cardSummryDto=new CardSummryDto();
 		List<CardSummary> cardSummary=cardSummarRepository.findCardDetail(customerId);
@@ -37,11 +43,11 @@ public class CardSummarServiceImpl implements CardSummaryService {
 
 	@Override
 	public CardDetailDto cardDetails(Long CustomerId) {
+		
+		logger.info("in cardDetails");
 
 		CardDetailDto cardDetailDto=new CardDetailDto();
-		System.out.println("in service");
 		CreditCard creditCard=creditCardRepository.findCustomerCardDetail(CustomerId);
-		System.out.println(creditCard.getCreditCardHolderName());
 		cardDetailDto.setCreditCardNumber(creditCard.getCardCardNumber());
 		cardDetailDto.setCreditCardType(creditCard.getCreditCardType());
 		cardDetailDto.setCreditLimit(creditCard.getCreditLimit());
